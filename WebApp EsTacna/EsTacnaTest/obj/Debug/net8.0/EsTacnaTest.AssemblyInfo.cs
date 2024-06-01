3c374ef3f892ddb2bbe96b43b43ab435ce886db3
Daphini-UPT <dangonzalezf@upt.pe>
2024-06-01 17:01:08 -0500

    Version 2.1

--- WebApp EsTacna/EsTacnaTest/obj/Debug/net8.0/EsTacnaTest.AssemblyInfo.cs
+++ WebApp EsTacna/EsTacnaTest/obj/Debug/net8.0/EsTacnaTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsTacnaTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad1cddcdb63e4997476adf3fa544232427f8652")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsTacnaTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsTacnaTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
